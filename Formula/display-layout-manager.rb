290fbe125114979da2943abe3407b39ba4d8da66
Eiji KOMINAMI <eiji@surbiton.jp>
2025-12-06 14:28:51 +0900

    Update to v1.1.1 with PyObjC dependencies
    - Update version to 1.1.1 with correct SHA256
    - Add pyobjc-core and pyobjc-framework-Cocoa resources
    - Add automatic daemon setup in post_install
    - Add uninstall_preflight to clean up daemon
    - Ensures PyObjC is properly installed via Homebrew

--- Formula/display-layout-manager.rb
+++ Formula/display-layout-manager.rb
@@ -11,6 +11,16 @@ class DisplayLayoutManager < Formula
   depends_on "jakehilborn/jakehilborn/displayplacer"
   depends_on "grep"
 
+  resource "pyobjc-core" do
+    url "https://files.pythonhosted.org/packages/b8/b6/d5612eb40be4fd5ef88c259339e6313f46ba67577a95d86c3470b951fce0/pyobjc_core-12.1.tar.gz"
+    sha256 "2bb3903f5387f72422145e1466b3ac3f7f0ef2e9960afa9bcd8961c5cbf8bd21"
+  end
+
+  resource "pyobjc-framework-Cocoa" do
+    url "https://files.pythonhosted.org/packages/02/a3/16ca9a15e77c061a9250afbae2eae26f2e1579eb8ca9462ae2d2c71e1169/pyobjc_framework_cocoa-12.1.tar.gz"
+    sha256 "5556c87db95711b985d5efdaaf01c917ddd41d148b1e52a0c66b1a2e2c5c1640"
+  end
+
   def install
     virtualenv_install_with_resources
   end
